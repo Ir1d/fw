838df526a2b06ad628bf6f6f5825843f398c8ad1
Ir1d <sirius.caffrey@gmail.com>
2019-11-06 01:35:08 +0800

    feat: add PixelUnShuffle and MemoryEfficientSwish

--- fw/__init__.py
+++ fw/__init__.py
@@ -1,1 +1,3 @@
 name = "fw"
+
+from .nn import *
---
+++ fw/nn/__init__.py
@@ -0,0 +1,4 @@
+from .activation import MemoryEfficientSwish
+from .misc import PixelUnShuffle
+
+__all__ = ['PixelUnShuffle', 'MemoryEfficientSwish']
