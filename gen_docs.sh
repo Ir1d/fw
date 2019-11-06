af6651a380b1d4230827a06bc989c69144c2a0a7
Ir1d <sirius.caffrey@gmail.com>
2019-11-06 16:23:24 +0800

    fix github authenticate

--- gen_docs.sh
+++ gen_docs.sh
@@ -6,4 +6,4 @@ git checkout -b gh-pages
 git add .
 git -c user.name='Ir1d' -c user.email='sirius.caffrey@gmail.com' commit -m init
   
-git push -f -q https://ir1d:$GITHUB_TOKEN@github.com/ir1d/fw.git gh-pages #&>/dev/null
+git push -f -q https://$GITHUB_TOKEN@github.com/ir1d/fw.git gh-pages &>/dev/null
